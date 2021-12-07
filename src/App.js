128d256505640b90a733fc4dd49b0fd42f7f8c46
Sidney Patez <instalers.sa@gmail.com>
2021-12-07 18:17:34 -0300

    feat: initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium
+        laoreet luctus. Donec sodales cursus eros quis lobortis. Donec consequat
+        in odio ac iaculis. Aenean id sapien vitae dolor fringilla imperdiet
+        vitae auctor risus. Nulla consectetur accumsan dolor, a cursus sem.
+        Suspendisse et dui eu mauris convallis posuere. Duis hendrerit lacus
+        eget eleifend porta. Phasellus pharetra elit erat, eu aliquam nibh
+        blandit ut. Etiam gravida lobortis lacus, in dapibus augue condimentum
+        efficitur. Morbi eu massa a metus vulputate vehicula ut ac nulla. Etiam
+        sit amet arcu in elit dignissim dictum at nec dui. Aliquam nunc nisi,
+        malesuada vel scelerisque quis, consectetur sed est. Cras luctus nunc
+        urna, eget suscipit elit ullamcorper suscipit. Vestibulum mollis mauris
+        at tincidunt placerat. Proin lacus odio, faucibus ut arcu sit amet,
+        convallis sodales sem. Sed nisi libero, tempor in gravida ut, facilisis
+        sit amet eros. In sodales lorem eu quam laoreet blandit. Etiam quis dui
+        neque. In consequat orci in justo iaculis, at aliquam orci faucibus.
+        Nullam molestie scelerisque nibh sed molestie. Nunc a ipsum purus.
+        Curabitur finibus arcu lectus, sed commodo risus tincidunt vel. Donec
+        non imperdiet leo. Donec odio lacus, porta non ex quis, tristique mattis
+        ligula.
+      </p>
+      <a href="#">uma tag a</a>
+      <Button>um botão</Button>
     </div>
   );
 }
